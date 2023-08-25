17080b913beecc16e82faaf813b37191499d2546
SpendIce <gjuanignacio@proton.me>
2023-08-25 08:15:52 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
+void swap(int* const p1, int* const p2) {
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
 
 int main() {
     int num = 10;
-    int* const p = &num;
-    cout << *p << endl;
-    *p = 20;
-    cout << *p << endl;
-    int const n2 = 30;
-    int* p2 = &n2;
-    cout << *p2 << endl;
-    *p2 = 40;
-    cout << *p2 << endl;
+    int num2 = 20;
+    cout << "a: " << num << " b: " << num2 << endl;
+    swap(&num, &num2);
+    cout << "a: " << num << " b: " << num2 << endl;
     return 0;
 }
