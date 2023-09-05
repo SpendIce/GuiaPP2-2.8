7248c914a248fde176fd3d877f690ead2233fa5f
SpendIce <gjuanignacio@proton.me>
2023-09-05 11:59:36 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -5,12 +5,20 @@ void swap(int* const p1, int* const p2) {
     *p1 = *p2;
     *p2 = temp;
 }
+int suma(int* vector, int tam)
+{
+    int sum = 0;
+    for (int i = 0; i < tam; i++)
+    {
+        sum += *vector;
+        vector++;
+    }
+    return sum;
+}
 
 int main() {
-    int num = 10;
-    int num2 = 20;
-    cout << "a: " << num << " b: " << num2 << endl;
-    swap(&num, &num2);
-    cout << "a: " << num << " b: " << num2 << endl;
+    int vector[5] = { 1,2,3,4,5 };
+    int* p = vector;
+    cout << "Suma: " << suma(p, 5) << endl;
     return 0;
 }
