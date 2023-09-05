4c8d67a3929c1520d09f3258f195b3505b371d8c
SpendIce <gjuanignacio@proton.me>
2023-09-05 12:32:14 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -5,20 +5,18 @@ void swap(int* const p1, int* const p2) {
     *p1 = *p2;
     *p2 = temp;
 }
-int suma(int* vector, int tam)
-{
-    int sum = 0;
-    for (int i = 0; i < tam; i++)
-    {
-        sum += *vector;
-        vector++;
+int* vectorDePares(int n) {
+    int *vector = new int[n]; // Asignar memoria dinámica para el arreglo
+    for (int i = 0, par = 2; i < n; i++, par+=2) {
+        vector[i] = par; // Llenar el arreglo con números pares
     }
-    return sum;
+    return vector;
 }
-
 int main() {
-    int vector[5] = { 1,2,3,4,5 };
-    int* p = vector;
-    cout << "Suma: " << suma(p, 5) << endl;
+    int num = 10;
+    int *pares = vectorDePares(num);
+    for (int i = 0; i < num; i++) {
+        cout << pares[i] << " ";
+    }
     return 0;
 }
