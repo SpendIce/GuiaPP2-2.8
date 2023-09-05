31afbd274251c52e6c9b9d277b1e3e83a225c9ea
SpendIce <gjuanignacio@proton.me>
2023-09-05 12:36:32 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int* vectorDePares(int n) {
-    int *vector = new int[n]; // Asignar memoria dinámica para el arreglo
-    for (int i = 0, par = 2; i < n; i++, par+=2) {
-        vector[i] = par; // Llenar el arreglo con números pares
-    }
-    return vector;
-}
 int main() {
-    int num = 10;
-    int *pares = vectorDePares(num);
-    for (int i = 0; i < num; i++) {
-        cout << pares[i] << " ";
+    int *p = new int[5] {1, 2, 3, 4, 5};
+    int suma = 0;
+    for (int i = 0; i < 5; i++) {
+        suma += *p;
+        p++;
     }
+    cout << "Suma: " << suma << endl;
     return 0;
 }
