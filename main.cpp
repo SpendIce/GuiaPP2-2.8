72d649d01b7ed1a8233d4f737020c1ed2d75cfaa
SpendIce <gjuanignacio@proton.me>
2023-09-06 08:18:06 -0300

    lo mismo con char* en vez de string

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int vocales(char* vector)
-{
-    int vocales = 0;
-    while(*vector != '\0')
-    {
-        if (*vector == 'a' || *vector == 'e' || *vector == 'i' || *vector == 'o' || *vector == 'u')
-        {
-            vocales++;
+void orden(char* *vector, int tam) {
+    char* aux;
+    for (int i = 1; i < tam; i++) {
+        if (vector[i][0] < vector[i-1][0]) {
+            aux = vector[i];
+            vector[i] = vector[i-1];
+            vector[i-1] = aux;
+            i = 0;
         }
-        vector++;
     }
-    return vocales;
 }
 
 int main() {
-    string palabra = "banana mundo";
-    cout << "La palabra " << palabra << " tiene " << vocales(&palabra[0]) << " vocales" << endl;
+    char* *vector = new char*[5];
+    char* pal1 = "hola";
+    char* pal2 = "mundo";
+    char* pal3 = "como";
+    char* pal4 = "estas";
+    char* pal5 = "hoy";
+    vector[0] = pal1;
+    vector[1] = pal2;
+    vector[2] = pal3;
+    vector[3] = pal4;
+    vector[4] = pal5;
+    orden(vector, 5);
+    for (int i = 0; i < 5; i++) {
+        cout << vector[i] << endl;
+    }
     return 0;
 }
