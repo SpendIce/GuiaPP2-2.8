1906588f39adffe1de2bb21512d23ecb081b0e61
SpendIce <gjuanignacio@proton.me>
2023-09-11 11:02:28 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+
 using namespace std;
 
-void orden(char* *vector, int tam) {
-    char* aux;
-    for (int i = 1; i < tam; i++) {
-        if (vector[i][0] < vector[i-1][0]) {
-            aux = vector[i];
-            vector[i] = vector[i-1];
-            vector[i-1] = aux;
-            i = 0;
-        }
+char* mayusculas(char* entrada, int tam) {
+    char* salida = new char[tam];
+    for (int i = 0; i < tam; i++) {
+        salida[i] = toupper(entrada[i]);
     }
+    return salida;
 }
 
 int main() {
-    char* *vector = new char*[5];
-    char* pal1 = "hola";
-    char* pal2 = "mundo";
-    char* pal3 = "como";
-    char* pal4 = "estas";
-    char* pal5 = "hoy";
-    vector[0] = pal1;
-    vector[1] = pal2;
-    vector[2] = pal3;
-    vector[3] = pal4;
-    vector[4] = pal5;
-    orden(vector, 5);
-    for (int i = 0; i < 5; i++) {
-        cout << vector[i] << endl;
-    }
+    char* palabra = new char[10];
+    palabra = "hola mundo   ";
+    cout << mayusculas(palabra, 10) << endl;
     return 0;
 }
