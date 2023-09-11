e763ba3777f93d1ce116a75426f7c7d97a50650c
SpendIce <gjuanignacio@proton.me>
2023-09-11 12:05:44 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -3,33 +3,27 @@
 #include <cctype>
 
 using namespace std;
-struct Fecha {
-    int dia;
-    int mes;
-    int anio;
-};
-struct Persona {
+//Declara un arreglo de punteros a estructuras Estudiante y llena el arreglo con información
+//de tres estudiantes. Luego, muestra los datos de todos los estudiantes utilizando los
+//punteros
+struct Estudiante {
     string nombre;
-    Fecha fechaNacimiento;
+    int edad;
+    float promedio;
 };
 
 int main() {
-    Persona* personas = new Persona[3];
-    personas[0].nombre = "Juan";
-    personas[0].fechaNacimiento.dia = 1;
-    personas[0].fechaNacimiento.mes = 1;
-    personas[0].fechaNacimiento.anio = 2000;
-    personas[1].nombre = "Pedro";
-    personas[1].fechaNacimiento.dia = 2;
-    personas[1].fechaNacimiento.mes = 2;
-    personas[1].fechaNacimiento.anio = 2001;
-    personas[2].nombre = "Maria";
-    personas[2].fechaNacimiento.dia = 3;
-    personas[2].fechaNacimiento.mes = 3;
-    personas[2].fechaNacimiento.anio = 2002;
+    Estudiante *ptrEstudiante[3] = {nullptr, nullptr, nullptr};
+    Estudiante estudiante1 = {"Juan", 20, 9.5};
+    Estudiante estudiante2 = {"Maria", 19, 9.0};
+    Estudiante estudiante3 = {"Pedro", 21, 8.5};
+    ptrEstudiante[0] = &estudiante1;
+    ptrEstudiante[1] = &estudiante2;
+    ptrEstudiante[2] = &estudiante3;
     for (int i = 0; i < 3; i++) {
-        cout << "Nombre: " << personas[i].nombre << endl;
-        cout << "Fecha de nacimiento: " << personas[i].fechaNacimiento.dia << "/" << personas[i].fechaNacimiento.mes << "/" << personas[i].fechaNacimiento.anio << endl;
+        cout << "Nombre: " << ptrEstudiante[i]->nombre << endl;
+        cout << "Edad: " << ptrEstudiante[i]->edad << endl;
+        cout << "Promedio: " << ptrEstudiante[i]->promedio << endl;
     }
     return 0;
 }
