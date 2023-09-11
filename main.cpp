7155132febdc49f7bc07d7b512af593508be2dd4
SpendIce <gjuanignacio@proton.me>
2023-09-11 11:42:58 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -3,36 +3,15 @@
 #include <cctype>
 
 using namespace std;
-int suma(int *a, int *b)
-{
-    return *a + *b;
-}
-int resta(int *a, int *b)
-{
-    return *a - *b;
-}
-int multiplicacion(int *a, int *b)
-{
-    return *a * *b;
-}
-int division(int *a, int *b)
-{
-    if(*b != 0)
-        return *a / *b;
-    else
-        cout << "No se puede dividir entre 0" << endl;
-    return 0;
-}
+struct Punto {
+    int x;
+    int y;
+};
+
 int main() {
-    int a = 5, b = 3;
-    int (*funcionOperacion)(int*, int*);
-    funcionOperacion = suma;
-    cout << "Suma: " << funcionOperacion(&a, &b) << endl;
-    funcionOperacion = resta;
-    cout << "Resta: " << funcionOperacion(&a, &b) << endl;
-    funcionOperacion = multiplicacion;
-    cout << "Multiplicacion: " << funcionOperacion(&a, &b) << endl;
-    funcionOperacion = division;
-    cout << "Division: " << funcionOperacion(&a, &b) << endl;
+    Punto *punto = new Punto;
+    punto->x = 1;
+    punto->y = 2;
+    cout << "Punto: " << punto->x << ", " << punto->y << endl;
     return 0;
 }
